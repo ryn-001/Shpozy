4c98582c20bf4ceb3557882cfab6ef5d304b643e
schizo <aryan.khaire001@gmail.com>
2025-09-09 00:14:37 +0530

    Shopzy Initiated

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import './Header.css';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import {Aavtar,Button,Stack} from "@mui/material";
+
+const Header = ({ children, hasHiddenButtons }) => {
+    const navigate = useNavigate();
+    const username = localStorage.getItem("username");
+
+    return (
+        <div className="header-wrapper">
+            <Box className="header">
+                <Box className="header-title">
+                    <img className="header-logo" src="/assets/shopzyLightTheme.png" alt="shopzy-light-theme-logo"></img>
+                </Box>
+
+                <Box className="buttons">
+                    {hasHiddenButtons ? (
+                        <Button
+                            className="expore-button"
+                            startIcon={<ArrowBackIcon/>}
+                            variant="text"
+                            onClick={() => {
+                                navigate("/")
+                            }}
+                        ></Button>
+                    ) : (
+                    <Stack>
+
+                    </Stack>)}
+                </Box>
+            </Box>
+        </div>
+    )
+}
+
+export default Header;
